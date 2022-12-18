f5c31b68e91b995bc62086bf1690c9d636aed308
dragec8 <dragutin.oreski@gmail.com>
2022-12-18 23:08:10 +0100

    feat: fix and improve eighteenth day solution

--- 2022/18_day/solution.py
+++ 2022/18_day/solution.py
@@ -1,6 +1,13 @@
-import functools
-from collections import Counter
-from typing import Iterator, Optional, List
+from typing import Iterator, Optional, Set, Tuple, List
+
+mask = [
+    (-1, 0, 0),
+    (1, 0, 0),
+    (0, -1, 0),
+    (0, 1, 0),
+    (0, 0, -1),
+    (0, 0, 1),
+]
 
 
 def _read_input_file(path: Optional[str] = 'input.txt') -> Iterator[str]:
@@ -9,99 +16,58 @@ def _read_input_file(path: Optional[str] = 'input.txt') -> Iterator[str]:
             yield line.strip('\n')
 
 
-def _get_sides(points: str) -> List:
-    point_1 = tuple(int(point) for point in points.split(','))
-    point_2 = (point_1[0] + 1, point_1[1], point_1[2])
-    point_3 = (point_1[0] + 1, point_1[1] + 1, point_1[2])
-    point_4 = (point_1[0], point_1[1] + 1, point_1[2])
-    point_5 = (point_1[0], point_1[1], point_1[2] + 1)
-    point_6 = (point_1[0] + 1, point_1[1], point_1[2] + 1)
-    point_7 = (point_1[0] + 1, point_1[1] + 1, point_1[2] + 1)
-    point_8 = (point_1[0], point_1[1] + 1, point_1[2] + 1)
-
-    side_1 = tuple(sorted((point_1, point_2, point_3, point_4)))
-    side_2 = tuple(sorted((point_1, point_2, point_5, point_6)))
-    side_3 = tuple(sorted((point_5, point_6, point_8, point_7)))
-    side_4 = tuple(sorted((point_4, point_3, point_8, point_7)))
-    side_5 = tuple(sorted((point_1, point_5, point_4, point_8)))
-    side_6 = tuple(sorted((point_2, point_6, point_3, point_7)))
-
-    return [
-        side_1,
-        side_2,
-        side_3,
-        side_4,
-        side_5,
-        side_6,
-    ]
-
-
-def process_input():
-    input_data = [line for line in _read_input_file()]
-    counter = Counter()
-    ans = 0
-    for point in input_data:
-        sides = _get_sides(point)
-        for side in sides:
-            if side not in counter:
-                ans += 1
-            else:
-                ans -= 1
-            counter[side] += 1
-
-    return ans
-
-
-ans = 0
-
-
-def process_input2():
+def _get_points():
     input_data = [line.split(',') for line in _read_input_file()]
-    points = sorted([(int(x), int(y), int(z)) for x, y, z in input_data])
+    return set(sorted([(int(x), int(y), int(z)) for x, y, z in input_data]))
 
-    @functools.lru_cache(None)
-    def dfs3(x, y, z) -> int:
-        global ans
-        if x > 22 or y > 22 or z > 22 or x < 0 or y < 0 or z < 0:
-            return 0
-        if (x + 1, y, z) in points:
-            ans += 1
-        else:
-            dfs3(x + 1, y, z)
-        if (x, y + 1, z) in points:
-            ans += 1
-        else:
-            dfs3(x, y + 1, z)
-        if (x, y, z + 1) in points:
-            ans += 1
-        else:
-            dfs3(x, y, z + 1)
 
-    @functools.lru_cache(None)
-    def dfs4(x, y, z) -> int:
-        global ans
-        if x > 22 or y > 22 or z > 22 or x < 0 or y < 0 or z < 0:
-            return 0
-        if (x - 1, y, z) in points:
-            ans += 1
-        else:
-            dfs4(x - 1, y, z)
-        if (x, y - 1, z) in points:
-            ans += 1
-        else:
-            dfs4(x, y - 1, z)
-        if (x, y, z - 1) in points:
-            ans += 1
-        else:
-            dfs4(x, y, z - 1)
-        return 0
+def calc_not_shared_surface_area(points: Set[Tuple[int, int, int]]) -> int:
+    ans = 0
 
-    dfs3(0, 0, 0)
-    dfs4(22, 22, 22)
+    for x, y, z in points:
+        ans += 6
+        for dx, dy, dz in mask:
+            if (x + dx, y + dy, z + dz) in points:
+                ans -= 1
 
     return ans
+
+
+def _get_air_pockets() -> List[Set[Tuple[int, int, int]]]:
+    active_points = _get_points()
+    all_points = {(x, y, z) for x in range(20) for y in range(20) for z in range(20)}
+    empty_points = list(all_points - active_points)
+    air_pockets = []
+
+    while empty_points:
+        points_to_check = [empty_points[0]]
+        current_air_pocket = set()
+
+        while len(points_to_check):
+            point = points_to_check.pop()
+
+            if point in empty_points:
+                current_air_pocket.add(point)
+                empty_points.remove(point)
+
+                x, y, z = point
+                for dx, dy, dz in mask:
+                    points_to_check.append((x + dx, y + dy, z + dz))
+
+        if (0, 0, 0) not in current_air_pocket:
+            air_pockets.append(current_air_pocket)
+
+    return air_pockets
+
+
+def calc_outer_surface_area() -> int:
+    active_points = _get_points()
+    air_pockets = _get_air_pockets()
+
+    return calc_not_shared_surface_area(active_points) -\
+           sum([calc_not_shared_surface_area(air_pocket) for air_pocket in air_pockets])
 
 
 if __name__ == "__main__":
-    print(process_input())
-    print(process_input2())
+    print(calc_not_shared_surface_area(_get_points()))
+    print(calc_outer_surface_area())
